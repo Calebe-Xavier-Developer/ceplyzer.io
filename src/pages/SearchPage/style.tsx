b87357702877f4f259e3f6d803e2fd2f29afb242
Calebe-Xavier-Developer <57916053+Calebe-Xavier-Developer@users.noreply.github.com>
2024-09-09 23:01:09 -0300

    Update style.tsx

--- src/pages/SearchPage/style.tsx
+++ src/pages/SearchPage/style.tsx
@@ -1,15 +1,9 @@
 import styled from 'styled-components';
 
 export const ContainerAddress = styled.div`
+  width: 90vw;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-around;
-
-  width: 90vw;
-  height: 90vh;
-
-  background-color: #fff;
-  box-shadow: 0px 0px 37px -11px rgba(0,0,0,0.46);
-  border-radius: 8px;
+  justify-content: center;
 `;
